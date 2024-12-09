2631a9eaafd3d924d8c45ec6ce02d0602651961d
woldemortq <166479426+woldemortq@users.noreply.github.com>
2024-12-09 11:47:38 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '192.168.31.11',
-    port: 5173,
-  }
 })
